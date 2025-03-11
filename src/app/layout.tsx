5b94a4f5f1376db31e3612f75c023e3d8f33413e
Kardamitsis Evan <kardamitsis.e@gmail.com>
2025-03-11 13:12:34 +0200

    Add favicon SVG to website metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'ISieveTech',
   description: 'The buzz-phrase for what we do is sentiment analysis, but what do we actually do? We crawl the web and identify relevant comments made by news media and by people in their blogs and micro-blogs (such as Twitter), in the comments they make about online videos, in discussion forums and, where publicly accessible, social networks. We use highly sophisticated techniques to identify and extract comments that are relevant to the question you asked and to classify them as being either positive, negative, neutral or balanced. i-sieve Technologies is able to discount references to your company that are irrelevant.',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.svg',
+        type: 'image/svg+xml',
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
